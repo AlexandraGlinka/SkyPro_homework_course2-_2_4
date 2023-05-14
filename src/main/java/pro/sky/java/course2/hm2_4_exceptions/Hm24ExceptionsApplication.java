221d09a894a5bb63c5edef865fe5d438b33e9c28
glinka <alexandraglinka@yandex.ru>
2023-05-14 22:38:11 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/course2/hm2_4_exceptions/Hm24ExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hm2_4_exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm24ExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hm24ExceptionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hm2_4_exceptions/Hm24ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hm2_4_exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hm24ExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
